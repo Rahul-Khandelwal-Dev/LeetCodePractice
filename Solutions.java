10ad35536650c12e69496893a2e3ec01491269bf
Rahul Khandelwal <rahul.khandelwal@prudential.com.sg>
2024-06-16 22:45:12 +0800

    Question number 330

--- Solutions.java
+++ Solutions.java
@@ -1,9 +1,12 @@
 import Questions.IPO;
+import Questions.PatchingArray;
 
 public class Solutions {
     public static void main(String args[]) {
         IPO ipo = new IPO();
+        PatchingArray patchingArray = new PatchingArray();
         System.out.println("Solutions");
         System.out.println("502) IPO" + ipo.findMaximizedCapital(2,0, new int[]{1, 2, 3}, new int[]{0, 1, 1}));
+        System.out.println("330) Patching Array" + patchingArray.minPatches( new int[]{1,3}, 6));
     }
 }
